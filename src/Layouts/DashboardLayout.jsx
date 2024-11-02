64099d51d80e204c3591ee69b4080bf640ef9b00
shakiqurrahman <rahmanshakiqur@gmail.com>
2024-11-02 18:34:07 +0600

    dashboard setup done

--- src/Layouts/DashboardLayout.jsx
+++ src/Layouts/DashboardLayout.jsx
@@ -1,15 +1,18 @@
 import React from "react";
 import { Toaster } from "react-hot-toast";
-import { ScrollRestoration } from "react-router-dom";
+import { Outlet, ScrollRestoration } from "react-router-dom";
 import DHeader from "../components/shared/Dashboard-header/DHeader";
+import DSideBar from "../components/shared/Dashboard-header/DSidebar";
 
 const DashboardLayout = () => {
     return (
         <>
             <DHeader />
             <div className="lg:flex gap-10">
-                {/* <DSideBar /> */}
-                {/* <main className="flex-1 mt-4 p-4 md:p-8">{children}</main> */}
+                <DSideBar />
+                <div className="flex-grow mt-4 p-4 md:p-8">
+                    <Outlet />
+                </div>
             </div>
             <ScrollRestoration />
             <Toaster position="top-center" />
--- src/components/shared/Dashboard-header/DSidebar.jsx
+++ src/components/shared/Dashboard-header/DSidebar.jsx
@@ -1,19 +1,16 @@
-"use client";
-import { setActiveHamburgerD } from "@/Redux/features/utilsSlice";
-import Link from "next/link";
-import { usePathname } from "next/navigation";
+import React from "react";
 import { BsFillPeopleFill } from "react-icons/bs";
 import { FaBookOpen } from "react-icons/fa";
-import { ImProfile } from "react-icons/im";
 import { MdCategory, MdDashboard } from "react-icons/md";
 import { useDispatch, useSelector } from "react-redux";
+import { NavLink } from "react-router-dom";
+import { setActiveHamburgerD } from "../../../Redux/features/utilsSlice";
 
 const DSideBar = () => {
-    const pathname = usePathname();
     const dispatch = useDispatch();
     const { activeHamburger } = useSelector((state) => state.utils);
     return (
-        <aside className="lg:w-[240px]  relative">
+        <aside className="lg:w-[240px] relative">
             <ul
                 className={`text-black bg-white w-full md:w-[250px]  left-0  fixed top-24 h-full pt-4  shadow-lg lg:translate-x-0 ${
                     activeHamburger
@@ -21,58 +18,42 @@ const DSideBar = () => {
                         : "translate-x-[-100%] duration-300"
                 }`}
             >
-                <Link href="/admin-dashboard">
+                <NavLink to="/admin-dashboard">
                     <li
-                        className={`link ${
-                            pathname === "/admin-dashboard"
-                                ? "text-primary"
-                                : ""
-                        } flex gap-4 text-[#7e7e7e] text-xl  px-8 py-5 hover:text-primary duration-300`}
+                        className={` flex gap-4 text-[#7e7e7e] text-xl  px-8 py-5 hover:text-primary duration-300`}
                         onClick={() => dispatch(setActiveHamburgerD(false))}
                     >
                         <MdDashboard size={25} />
                         Dashboard
                     </li>
-                </Link>
-                <Link href="/admin-dashboard/stories">
+                </NavLink>
+                <NavLink to="/admin-dashboard/stories">
                     <li
-                        className={`link ${
-                            pathname === "/admin-dashboard/stories"
-                                ? "text-primary "
-                                : ""
-                        } flex items-center gap-4 text-[#7e7e7e] text-xl  px-8 py-5 hover:text-primary duration-300`}
+                        className={` flex items-center gap-4 text-[#7e7e7e] text-xl  px-8 py-5 hover:text-primary duration-300`}
                         onClick={() => dispatch(setActiveHamburgerD(false))}
                     >
                         <FaBookOpen size={25} />
                         Stories
                     </li>
-                </Link>
-                <Link href="/admin-dashboard/authors">
+                </NavLink>
+                <NavLink to="/admin-dashboard/authors">
                     <li
-                        className={`link ${
-                            pathname === "/admin-dashboard/authors"
-                                ? "text-primary "
-                                : ""
-                        } flex items-center gap-4 text-[#7e7e7e] text-xl  px-8 py-5 hover:text-primary duration-300`}
+                        className={` flex items-center gap-4 text-[#7e7e7e] text-xl  px-8 py-5 hover:text-primary duration-300`}
                         onClick={() => dispatch(setActiveHamburgerD(false))}
                     >
                         <BsFillPeopleFill size={25} />
                         Authors
                     </li>
-                </Link>
-                <Link href="/admin-dashboard/categories">
+                </NavLink>
+                <NavLink to="/admin-dashboard/categories">
                     <li
-                        className={`link ${
-                            pathname === "/admin-dashboard/categories"
-                                ? "text-primary "
-                                : ""
-                        } flex items-center gap-4 text-[#7e7e7e] text-xl  px-8 py-5 hover:text-primary duration-300`}
+                        className={` flex items-center gap-4 text-[#7e7e7e] text-xl  px-8 py-5 hover:text-primary duration-300`}
                         onClick={() => dispatch(setActiveHamburgerD(false))}
                     >
                         <MdCategory size={25} />
                         Categories
                     </li>
-                </Link>
+                </NavLink>
             </ul>
         </aside>
     );
