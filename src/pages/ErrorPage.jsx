045672b4d5721d48d729dd6ff92892866b6ec70e
shakiqurrahman <rahmanshakiqur@gmail.com>
2024-11-02 19:44:38 +0600

    error page updated

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -6,7 +6,9 @@ import error from "../assets/images/error.jpg";
 const ErrorPage = () => {
     return (
         <div className="flex flex-col justify-center items-center h-screen">
-            <img className="w-[85%] sm:w-[400px]" src={error} alt="error" />
+            <div className="w-[85%] sm:w-[400px]">
+                <img className="w-full" src={error} alt="error" />
+            </div>
             <Link
                 to="/"
                 className="mt-2 flex items-center gap-2 text-base font-semibold text-white bg-black px-5 py-2 borde border-[#ff644d] rounded-lg hover:underline"
