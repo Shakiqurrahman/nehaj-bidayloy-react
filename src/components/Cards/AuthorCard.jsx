5ba78fa7ad78390ae7a0bd9615844c7992dc1783
Shakil Ahmed <itsdevelopershakil@gmail.com>
2025-02-13 20:29:33 +0600

    authors page api integration done

--- src/components/Cards/AuthorCard.jsx
+++ src/components/Cards/AuthorCard.jsx
@@ -1,16 +1,15 @@
 import React from "react";
-import authorsImage from "../../assets/images/author card.png";
 
-const AuthorCard = ({ authorsData }) => {
+const AuthorCard = ({ authorData }) => {
   return (
     <div>
       <img
         className="mb-3 rounded-2xl w-full object-cover object-top"
-        src={authorsImage}
+        src={authorData?.avatar?.url}
         alt="Authors Picture"
       />
       <p className="text-base sm:text-lg text-primary-blue font-bold">
-        আবুল বারাকাত
+        {authorData?.fullName}
       </p>
     </div>
   );
--- src/pages/AuthorsPage.jsx
+++ src/pages/AuthorsPage.jsx
@@ -1,8 +1,11 @@
 import React, { useState } from "react";
 import AuthorCard from "../components/Cards/AuthorCard";
 import Pagination from "../components/Pagination";
+import { useFetchAuthorsQuery } from "../Redux/api/authorApiSlice";
 
 const AuthorsPage = () => {
+  const { data: authors, isLoading } = useFetchAuthorsQuery();
+
   const [currentPage, setCurrentPage] = useState(1);
 
   const handlePageChange = (page) => {
@@ -14,17 +17,21 @@ const AuthorsPage = () => {
       <div>
         <h1 className="section_title">নেহাজের লেখকবৃন্দ</h1>
         <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-4 sm:gap-8">
-          {Array.from({ length: 10 }).map((_, i) => (
-            <AuthorCard key={i} />
+          {authors?.map((author, i) => (
+            <AuthorCard authorData={author} key={i} />
           ))}
         </div>
-        <hr className="bg-[#EAECF0] mb-5 mt-10" />
-        <Pagination
-          currentPage={currentPage}
-          itemsPerPage={10}
-          onPageChange={handlePageChange}
-          totalItems={100}
-        />
+        {authors?.length > 20 && (
+          <>
+            <hr className="bg-[#EAECF0] mb-5 mt-10" />
+            <Pagination
+              currentPage={currentPage}
+              itemsPerPage={10}
+              onPageChange={handlePageChange}
+              totalItems={100}
+            />
+          </>
+        )}
       </div>
     </section>
   );
