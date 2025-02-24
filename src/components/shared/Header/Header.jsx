37e9ee3ac39e4dff2e030016bf53daf4c4707d16
Shakil Ahmed <itsdevelopershakil@gmail.com>
2025-02-24 18:58:53 +0600

    header mobile view updated

--- src/components/shared/Header/Header.jsx
+++ src/components/shared/Header/Header.jsx
@@ -2,17 +2,23 @@ import React from "react";
 import { GiHamburgerMenu } from "react-icons/gi";
 import { Link, NavLink } from "react-router-dom";
 import Logo from "/public/Nehaj_Logo.png";
+import Icon from "/public/icon.png";
 
 const Header = () => {
   return (
     <header className="w-full absolute top-[75px] left-0 z-[99999999] px-[30px] sm:px-[46px]">
       <nav className="max-width font-niladri">
-        <div className="rounded-[100px] bg-primary-white flex items-center justify-between px-2 sm:px-6 py-[10px] lg:px-[30px] lg:py-4 border">
+        <div className="rounded-[100px] bg-primary-white flex items-center justify-between px-6 py-[10px] lg:px-[30px] lg:py-4 border">
           <Link to={"/"} className="shrink-0">
             <img
               src={Logo}
               alt="Nehaj Bidyaloy Logo"
-              className="w-[80px] sm:w-[165px] object-contain"
+              className="w-[80px] sm:block hidden sm:w-[165px] object-contain"
+            />
+            <img
+              src={Icon}
+              alt="Nehaj Bidyaloy Logo"
+              className="w-[50px] block sm:hidden sm:w-[165px] object-contain"
             />
           </Link>
           <ul className="hidden lg:flex items-center gap-10">
@@ -71,9 +77,9 @@ const Header = () => {
               যোগাযোগ
             </NavLink>
             <button className="shrink-0 block lg:hidden">
-              <GiHamburgerMenu className="w-[18px] h-4 text-primary-blue hover:text-primary-golden duration-300" />
+              <GiHamburgerMenu className="text-2xl text-primary-blue hover:text-primary-golden duration-300" />
             </button>
-            <button className="shrink-0 px-5 sm:px-10 py-1 text-sm sm:text-base sm:py-[15px] rounded-[40px] bg-primary-blue text-white hover:bg-primary-golden duration-300">
+            <button className="hidden sm:block shrink-0 px-5 sm:px-10 py-1 text-sm sm:text-base sm:py-[15px] rounded-[40px] bg-primary-blue text-white hover:bg-primary-golden duration-300">
               খুঁজুন
             </button>
           </div>
