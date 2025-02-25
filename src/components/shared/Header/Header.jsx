2c55d6877b8b888391f39cbc1855c3177cc3c818
Shakil Ahmed <itsdevelopershakil@gmail.com>
2025-02-25 21:21:04 +0600

    sidepanel added

--- src/components/shared/Header/Header.jsx
+++ src/components/shared/Header/Header.jsx
@@ -1,91 +1,170 @@
-import React from "react";
+import React, { useState } from "react";
 import { GiHamburgerMenu } from "react-icons/gi";
+import { IoIosArrowDown } from "react-icons/io";
 import { Link, NavLink } from "react-router-dom";
+import SidePanel from "./SidePanel";
 import Logo from "/public/Nehaj_Logo.png";
 import Icon from "/public/icon.png";
 
 const Header = () => {
+  const [toggle, setToggle] = useState(false);
   return (
-    <header className="w-full absolute top-[75px] left-0 z-[99999999] px-[30px] sm:px-[46px]">
-      <nav className="max-width font-niladri">
-        <div className="rounded-[100px] bg-primary-white flex items-center justify-between px-6 py-[10px] lg:px-[30px] lg:py-4 border">
-          <Link to={"/"} className="shrink-0">
-            <img
-              src={Logo}
-              alt="Nehaj Bidyaloy Logo"
-              className="w-[80px] sm:block hidden sm:w-[165px] object-contain"
-            />
-            <img
-              src={Icon}
-              alt="Nehaj Bidyaloy Logo"
-              className="w-[50px] block sm:hidden sm:w-[165px] object-contain"
-            />
-          </Link>
-          <ul className="hidden lg:flex items-center gap-10">
-            <li>
+    <>
+      <header className="w-full absolute top-[75px] left-0 z-[99999999] px-[30px] sm:px-[46px]">
+        <nav className="max-width font-niladri">
+          <div className="rounded-[100px] bg-primary-white flex items-center justify-between px-6 py-[10px] lg:px-[30px] lg:py-4 border">
+            <Link to={"/"} className="shrink-0">
+              <img
+                src={Logo}
+                alt="Nehaj Bidyaloy Logo"
+                className="w-[80px] sm:block hidden sm:w-[165px] object-contain"
+              />
+              <img
+                src={Icon}
+                alt="Nehaj Bidyaloy Logo"
+                className="w-[50px] block sm:hidden sm:w-[165px] object-contain"
+              />
+            </Link>
+            <ul className="hidden lg:flex items-center gap-6 xl:gap-10">
+              <li className="relative group">
+                <div className="text-lg text-primary-blue hover:text-primary-golden duration-300 flex items-center gap-3 cursor-pointer">
+                  বিষয় <IoIosArrowDown />
+                </div>
+                <ul className="absolute top-[32px] left-0 space-y-3 bg-white w-[150px] py-5 rounded-lg shadow group-hover:opacity-100 opacity-0 invisible group-hover:visible duration-300">
+                  <li>
+                    <NavLink
+                      to={"/category/thought"}
+                      className="text-lg text-primary-blue hover:text-primary-golden duration-300 px-3 block w-full hover:pl-5"
+                    >
+                      চিন্তা
+                    </NavLink>
+                  </li>
+                  <li>
+                    <NavLink
+                      to={"/category/history"}
+                      className="text-lg text-primary-blue hover:text-primary-golden duration-300 px-3 block w-full hover:pl-5"
+                    >
+                      ইতিহাস
+                    </NavLink>
+                  </li>
+                  <li>
+                    <NavLink
+                      to={"/category/literary"}
+                      className="text-lg text-primary-blue hover:text-primary-golden duration-300 px-3 block w-full hover:pl-5"
+                    >
+                      সাহিত্য
+                    </NavLink>
+                  </li>
+                  <li>
+                    <NavLink
+                      to={"/category/art"}
+                      className="text-lg text-primary-blue hover:text-primary-golden duration-300 px-3 block w-full hover:pl-5"
+                    >
+                      শিল্প
+                    </NavLink>
+                  </li>
+                  <li>
+                    <NavLink
+                      to={"/category/cinema"}
+                      className="text-lg text-primary-blue hover:text-primary-golden duration-300 px-3 block w-full hover:pl-5"
+                    >
+                      সিনেমা
+                    </NavLink>
+                  </li>
+                  <li>
+                    <NavLink
+                      to={"/category/theology"}
+                      className="text-lg text-primary-blue hover:text-primary-golden duration-300 px-3 block w-full hover:pl-5"
+                    >
+                      ধর্মতত্ত্ব
+                    </NavLink>
+                  </li>
+                </ul>
+              </li>
+              <li className="relative group">
+                <div className="text-lg text-primary-blue hover:text-primary-golden duration-300 flex items-center gap-3 cursor-pointer">
+                  ধরণ <IoIosArrowDown />
+                </div>
+                <ul className="absolute top-[32px] left-0 space-y-3 bg-white w-[120px] py-5 rounded-lg shadow group-hover:opacity-100 opacity-0 invisible group-hover:visible duration-300">
+                  <li>
+                    <NavLink
+                      to={"/genre/article"}
+                      className="text-lg text-primary-blue hover:text-primary-golden duration-300 px-3 block w-full hover:pl-5"
+                    >
+                      প্রবন্ধ
+                    </NavLink>
+                  </li>
+                  <li>
+                    <NavLink
+                      to={"/genre/notes"}
+                      className="text-lg text-primary-blue hover:text-primary-golden duration-300 px-3 block w-full hover:pl-5"
+                    >
+                      নোটস
+                    </NavLink>
+                  </li>
+                  <li>
+                    <NavLink
+                      to={"/genre/translate"}
+                      className="text-lg text-primary-blue hover:text-primary-golden duration-300 px-3 block w-full hover:pl-5"
+                    >
+                      অনুবাদ
+                    </NavLink>
+                  </li>
+                </ul>
+              </li>
+              <li>
+                <NavLink
+                  to={"/study-circle"}
+                  className="text-lg text-primary-blue hover:text-primary-golden duration-300"
+                >
+                  পাঠচক্র
+                </NavLink>
+              </li>
+              <li>
+                <NavLink
+                  to={"/author"}
+                  className="text-lg text-primary-blue hover:text-primary-golden duration-300"
+                >
+                  লেখক
+                </NavLink>
+              </li>
+              <li>
+                <NavLink
+                  to={"/about-us"}
+                  className="text-lg text-primary-blue hover:text-primary-golden duration-300"
+                >
+                  পরিচয়
+                </NavLink>
+              </li>
+            </ul>
+            <div className="flex items-center gap-[15px] xl:gap-[50px]">
               <NavLink
-                to={"/category"}
-                className="text-lg text-primary-blue hover:text-primary-golden duration-300"
+                to={"/notice"}
+                className="hidden sm:block text-lg text-primary-blue hover:text-primary-golden duration-300"
               >
-                বিষয়
+                বিজ্ঞপ্তি
               </NavLink>
-            </li>
-            <li>
               <NavLink
-                to={"/genre"}
-                className="text-lg text-primary-blue hover:text-primary-golden duration-300"
+                to={"/contact"}
+                className="hidden sm:block text-lg text-primary-blue hover:text-primary-golden duration-300"
               >
-                ধরণ
+                যোগাযোগ
               </NavLink>
-            </li>
-            <li>
-              <NavLink
-                to={"/study-circle"}
-                className="text-lg text-primary-blue hover:text-primary-golden duration-300"
+              <button
+                className="shrink-0 block lg:hidden"
+                onClick={() => setToggle(true)}
               >
-                পাঠচক্র
-              </NavLink>
-            </li>
-            <li>
-              <NavLink
-                to={"/author"}
-                className="text-lg text-primary-blue hover:text-primary-golden duration-300"
-              >
-                লেখক
-              </NavLink>
-            </li>
-            <li>
-              <NavLink
-                to={"/about-us"}
-                className="text-lg text-primary-blue hover:text-primary-golden duration-300"
-              >
-                পরিচয়
-              </NavLink>
-            </li>
-          </ul>
-          <div className="flex items-center gap-[15px] lg:gap-[50px]">
-            <NavLink
-              to={"/notice"}
-              className="hidden sm:block text-lg text-primary-blue hover:text-primary-golden duration-300"
-            >
-              বিজ্ঞপ্তি
-            </NavLink>
-            <NavLink
-              to={"/contact"}
-              className="hidden sm:block text-lg text-primary-blue hover:text-primary-golden duration-300"
-            >
-              যোগাযোগ
-            </NavLink>
-            <button className="shrink-0 block lg:hidden">
-              <GiHamburgerMenu className="text-2xl text-primary-blue hover:text-primary-golden duration-300" />
-            </button>
-            <button className="hidden sm:block shrink-0 px-5 sm:px-10 py-1 text-sm sm:text-base sm:py-[15px] rounded-[40px] bg-primary-blue text-white hover:bg-primary-golden duration-300">
-              খুঁজুন
-            </button>
+                <GiHamburgerMenu className="text-2xl text-primary-blue hover:text-primary-golden duration-300" />
+              </button>
+              <button className="hidden sm:block shrink-0 px-5 sm:px-10 py-1 text-sm sm:text-base sm:py-[15px] rounded-[40px] bg-primary-blue text-white hover:bg-primary-golden duration-300">
+                খুঁজুন
+              </button>
+            </div>
           </div>
-        </div>
-      </nav>
-    </header>
+        </nav>
+      </header>
+      <SidePanel open={toggle} close={() => setToggle(false)} />
+    </>
   );
 };
 
--- src/components/shared/Header/SidePanel.jsx
+++ src/components/shared/Header/SidePanel.jsx
@@ -1,7 +1,209 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
+import { IoIosArrowDown } from "react-icons/io";
+import { IoClose } from "react-icons/io5";
+import { Link, NavLink } from "react-router-dom";
+import useOutsideClick from "../../../hooks/useOutsideClick";
+import Logo from "/public/icon.png";
 
-const SidePanel = () => {
-  return <div></div>;
+const SidePanel = ({ open, close }) => {
+  const boxRef = useRef(null);
+  const dropdownsRef = useRef({});
+
+  const [openDropdown, setOpenDropdown] = useState("category");
+
+  useEffect(() => {
+    if (open) {
+      document.body.style.overflow = "hidden";
+    } else {
+      document.body.style.overflow = "auto";
+    }
+  }, [open]);
+
+  useOutsideClick(boxRef, close);
+
+  return (
+    <div
+      className={`h-screen lg:hidden block w-full bg-black/50 fixed inset-0 z-[99999999999] ${
+        open ? "visible opacity-100" : "invisible opacity-0"
+      }`}
+    >
+      <div
+        ref={boxRef}
+        className={`h-full w-full max-w-[360px] p-5 bg-white ml-auto duration-300 ${
+          open ? "translate-x-0" : "translate-x-full"
+        }`}
+      >
+        <div className="flex items-center gap-3">
+          <Link to={"/"}>
+            <img src={Logo} alt="Nehaj Logo" />
+          </Link>
+          <button className="ml-auto shrink-0 px-5 py-2 text-sm rounded-[40px] bg-primary-blue text-white hover:bg-primary-golden duration-300 font-niladri">
+            খুঁজুন
+          </button>
+          <button onClick={close} className="shrink-0">
+            <IoClose className="text-2xl" />
+          </button>
+        </div>
+        <span className="block h-[2px] w-full bg-primary-blue rounded-lg my-5"></span>
+        <ul className="font-niladri space-y-3 overflow-y-auto">
+          <li className="relative group overflow-hidden">
+            <div
+              className="text-lg text-primary-blue hover:text-primary-golden duration-300 flex items-center gap-3 cursor-pointer"
+              onClick={() =>
+                setOpenDropdown(openDropdown === "category" ? "" : "category")
+              }
+            >
+              বিষয় <IoIosArrowDown />
+            </div>
+            <ul
+              ref={(el) => (dropdownsRef.current["category"] = el)}
+              className="*:py-2 duration-300"
+              style={{
+                height:
+                  openDropdown === "category"
+                    ? dropdownsRef?.current["category"]?.scrollHeight + "px"
+                    : 0,
+              }}
+            >
+              <li>
+                <NavLink
+                  to={"/category/thought"}
+                  className="text-lg text-primary-blue hover:text-primary-golden duration-300 px-3 block w-full"
+                >
+                  চিন্তা
+                </NavLink>
+              </li>
+              <li>
+                <NavLink
+                  to={"/category/history"}
+                  className="text-lg text-primary-blue hover:text-primary-golden duration-300 px-3 block w-full"
+                >
+                  ইতিহাস
+                </NavLink>
+              </li>
+              <li>
+                <NavLink
+                  to={"/category/literary"}
+                  className="text-lg text-primary-blue hover:text-primary-golden duration-300 px-3 block w-full"
+                >
+                  সাহিত্য
+                </NavLink>
+              </li>
+              <li>
+                <NavLink
+                  to={"/category/art"}
+                  className="text-lg text-primary-blue hover:text-primary-golden duration-300 px-3 block w-full"
+                >
+                  শিল্প
+                </NavLink>
+              </li>
+              <li>
+                <NavLink
+                  to={"/category/cinema"}
+                  className="text-lg text-primary-blue hover:text-primary-golden duration-300 px-3 block w-full"
+                >
+                  সিনেমা
+                </NavLink>
+              </li>
+              <li>
+                <NavLink
+                  to={"/category/theology"}
+                  className="text-lg text-primary-blue hover:text-primary-golden duration-300 px-3 block w-full"
+                >
+                  ধর্মতত্ত্ব
+                </NavLink>
+              </li>
+            </ul>
+          </li>
+          <li className="relative group overflow-hidden">
+            <div
+              className="text-lg text-primary-blue hover:text-primary-golden duration-300 flex items-center gap-3 cursor-pointer"
+              onClick={() =>
+                setOpenDropdown(openDropdown === "genre" ? "" : "genre")
+              }
+            >
+              ধরণ <IoIosArrowDown />
+            </div>
+            <ul
+              ref={(el) => (dropdownsRef.current["genre"] = el)}
+              className="*:py-2 duration-300"
+              style={{
+                height:
+                  openDropdown === "genre"
+                    ? dropdownsRef?.current["genre"]?.scrollHeight + "px"
+                    : 0,
+              }}
+            >
+              <li>
+                <NavLink
+                  to={"/genre/article"}
+                  className="text-lg text-primary-blue hover:text-primary-golden duration-300 px-3 block w-full"
+                >
+                  প্রবন্ধ
+                </NavLink>
+              </li>
+              <li>
+                <NavLink
+                  to={"/genre/notes"}
+                  className="text-lg text-primary-blue hover:text-primary-golden duration-300 px-3 block w-full"
+                >
+                  নোটস
+                </NavLink>
+              </li>
+              <li>
+                <NavLink
+                  to={"/genre/translate"}
+                  className="text-lg text-primary-blue hover:text-primary-golden duration-300 px-3 block w-full"
+                >
+                  অনুবাদ
+                </NavLink>
+              </li>
+            </ul>
+          </li>
+          <li>
+            <NavLink
+              to={"/study-circle"}
+              className="text-lg text-primary-blue hover:text-primary-golden duration-300 block w-full"
+            >
+              পাঠচক্র
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to={"/author"}
+              className="text-lg text-primary-blue hover:text-primary-golden duration-300 block w-full"
+            >
+              লেখক
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to={"/about-us"}
+              className="text-lg text-primary-blue hover:text-primary-golden duration-300 block w-full"
+            >
+              পরিচয়
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to={"/notice"}
+              className="block text-lg text-primary-blue hover:text-primary-golden duration-300 w-full"
+            >
+              বিজ্ঞপ্তি
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to={"/contact"}
+              className="block text-lg text-primary-blue hover:text-primary-golden duration-300 w-full"
+            >
+              যোগাযোগ
+            </NavLink>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default SidePanel;
