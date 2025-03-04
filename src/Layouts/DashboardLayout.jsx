ad4f34d7ad8444e0c19ae6a3a327ecdf25de8ea1
Shakil Ahmed <itsdevelopershakil@gmail.com>
2025-03-05 03:44:18 +0600

    dashboard layout mobile view overflow bug fixed

--- src/Layouts/DashboardLayout.jsx
+++ src/Layouts/DashboardLayout.jsx
@@ -4,18 +4,18 @@ import DHeader from "../components/shared/Dashboard-header/DHeader";
 import DSideBar from "../components/shared/Dashboard-header/DSidebar";
 
 const DashboardLayout = () => {
-    return (
-        <section className="font-poppins bg-[#f2f2f2]">
-            <DHeader />
-            <div className="lg:flex gap-10">
-                <DSideBar />
-                <div className="flex-1 mt-4 p-4 md:p-8">
-                    <Outlet />
-                </div>
-            </div>
-            <ScrollRestoration />
-        </section>
-    );
+  return (
+    <section className="font-poppins bg-[#f2f2f2]">
+      <DHeader />
+      <div className="lg:flex gap-10">
+        <DSideBar />
+        <div className="flex-1 mt-4 p-4 md:p-8 overflow-y-auto">
+          <Outlet />
+        </div>
+      </div>
+      <ScrollRestoration />
+    </section>
+  );
 };
 
 export default DashboardLayout;
